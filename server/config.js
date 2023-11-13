60171679da585a3dbdff42562618aa44bd54e987
TiboulVinci <149047748+TiboulVinci@users.noreply.github.com>
2023-11-13 13:09:47 +0100

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 module.exports={
     dbfile:"timesheets.db",
-    emails:["@domain.com"],
-    secret:"changethissecretandkeepitprotected",
+    emails:["@vinci.be","@student.vinci.be"],
+    secret:"CXOM8DezKRyOsmk2zVQbddggyyuiAZN",
     port:8080,
     websocket:true,
     maxupload:100*1024*1024,
